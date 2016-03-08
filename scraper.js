090f2cde4fcc51e36b718d8db9758318fbaef772
Paul Santos <paulmichael.a.santos@gmail.com>
2016-03-08 00:06:03 -0500

    successfully scraped hackernews

--- scraper.js
+++ scraper.js
@@ -5,10 +5,15 @@ var cheerio = require('cheerio');
 request('https://news.ycombinator.com/', function (error, response, html) {
   var $ = cheerio.load(html);
   var result = [];
-  $([YOUR SELECTOR HERE]).each(function(i, element){
-
+  $('td.title:nth-child(3)>a').each(function(i, element){
+    // console.log($(element).children('.comhead > a > .sitestr'));
     //scrape some stuff, put it in an object and add it to the result array
-
+    var scraped = {
+      title: $(element).text(),
+      link: $(element).attr('href')
+      // from: $(element).children('.comhead > a > .sitestr').text()
+    };
+    result.push(scraped);
     });
   console.log(result);
 });
